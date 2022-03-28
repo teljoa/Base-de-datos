5ee2c76a79a812c15ba83195399a6c2aaa53bc30
Joaquín García Téllez <102540516+teljoa@users.noreply.github.com>
2022-03-28 18:17:59 +0200

    Add files via upload

---
+++ PECES/BOLETIN 2.sql
@@ -0,0 +1,28 @@
+--1. Mostrar el nombre y apellidos de todos los afiliados que tengan una licencia que empieza por A.
+
+--2. Mostrar los nombres de los peces que se han capturado en los eventos celebrados durante el año de 1998 indicando el nombre 
+--de la comunidad en la que se celebraron junto con el nombre y apellido del afiliado que lo capturó. La información debe 
+--aparecer ordenada por comunidad, luego por peces y por último por apellido del afiliado.
+
+--3. Mostrar los eventos, el lugar y los cauces en los que se han celebrado eventos internacionales (el nombre del evento 
+--contiene la palabra internacional en mayúsculas o minúsculas). Hay que hacer esta sentencia con JOIN.
+
+--4. Para cada uno de los peces que ha sido pescado por un afiliado en solitario, mostrar el nombre del pez, la talla, la fecha 
+--de pesca y la hora de la pesca, mostrando los datos ordenador por peces y luego los más grandes.
+
+--5. Mostrar todos los cauces en los que alguna vez algún afiliado ha pescado alguna vez un pez en solitario, siempre que el la 
+--relación talla/peso sea mayor que 3.
+
+--6. Mostrar el nombre y el apellido de los afiliados que han pescado algún pez en un evento y que en el campo de observaciones 
+--esté recogido que su hábitat es ríos.
+
+--7. Mostrar el nombre y el apellido del afiliado o afiliados que ha sido el primer avalador del afiliado con código 1000. 
+
+--8. Mostrar los eventos que se han celebrado en un lugar en el que el campo de observaciones no tiene valor.
+
+--9. Muestra el nombre y apellidos de todos las parejas de avales que existen en la base de datos. Es decir, debes mostrar el 
+--nombre y apellido del primer aval y el nombre y apellido del segundo aval.
+
+--10. Mostrar el nombre y apellido del afiliado o afiliados que han quedado en algunas de las cuatro primeras posiciones en algún 
+--evento o que participado en algún evento celebrado en el Coto De Dilar  o en el Coto De Fardes. (hay que hacer esta consulta 
+--sin utilizar JOIN)
